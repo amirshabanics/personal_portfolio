adfe92af5b8e039af38ea3b1b06f82ea2d3a5be6
farzinnasiri <farzin-nasiri@hotmail.com>
2020-07-31 09:06:46 +0300

    set index & detail for each projects in url /projects/

--- projects/views.py
+++ projects/views.py
@@ -15,4 +15,4 @@ def project_detail(request, pk):
     context = {
         'project': project
     }
-    return render(request, 'prject_detail.html', context)
+    return render(request, 'project_detail.html', context)
